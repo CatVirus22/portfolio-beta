859d74e9d50e8d2ad53864f98eae5ec766786d77
Marizabelle Aldana <94816917+CatVirus22@users.noreply.github.com>
2022-03-16 23:58:14 +0000

    placed greeting into container
    added JS document
    CSS changes made on $greeting, .media, h3, .mb-3
    corrected missing container in index

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const fade = document.querySelectorAll(".fade-in");
+
+const appearOption = { threshold: 1 };
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOption);
+
+fade.forEach((fade) => {
+  appearOnScroll.observe(fade);
+});
